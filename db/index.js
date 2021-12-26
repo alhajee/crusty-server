7f46fa57d2b653e809b1827adc16789ffc36d24a
Muhammad Alhajee <alhajee2009@gmail.com>
2021-12-26 20:19:42 +0100

    create database connection

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+// use when starting application locally
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+// use when starting application as docker container
+let mongoUrlDocker = "mongodb://admin:password@mongodb";
+
+// "user-account" in demo with docker. "my-db" in demo with docker-compose
+let databaseName = "my-db";
+
+mongoose
+    .connect(`${mongoUrlLocal}`, { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
