537e8d0317988fab40b20e8f9faf2a29dd27ec3a
Muhammad Alhajee <alhajee2009@gmail.com>
2021-12-26 21:17:36 +0100

    create grocery router

---
+++ routes/grocery-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const GroceryCtrl = require('../controllers/grocery-ctrl')
+
+const router = express.Router()
+
+router.post('/grocery', GroceryCtrl.createGrocery)
+router.put('/grocery/:id', GroceryCtrl.updateGrocery)
+router.delete('/grocery/:id', GroceryCtrl.deleteGrocery)
+router.get('/grocery/:id', GroceryCtrl.getGroceryById)
+router.get('/groceries', GroceryCtrl.getGroceries)
+
+module.exports = router
