03f8de2506433f3e09ab3f9c249a052b15a3c8dc
Muhammad Alhajee <alhajee2009@gmail.com>
2021-12-27 11:41:22 +0100

    modify grocery schema to include min/max validation

--- models/grocery-model.js
+++ models/grocery-model.js
@@ -3,12 +3,28 @@ const Schema = mongoose.Schema
 
 const Grocery = new Schema(
     {
-        name: {type: String, required: true},
-        unit: {type: String, required: false},
-        quantity: {type: Number, required: false},
-        price: {type: Number, required: true},
+        name: {
+            type: String, 
+            required: true
+        },
+        unit: {
+            type: String, 
+            required: false
+        },
+        quantity: {
+            type: Number, 
+            min: 1,
+            required: false
+        },
+        price: {
+            type: Number, 
+            min: 10,
+            required: true
+        },
     },
-    { timestamps: true},
+    { 
+        timestamps: true
+    },
 )
 
 module.exports = mongoose.model('Groceries', Grocery)
