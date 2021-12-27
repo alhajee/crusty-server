e9e4f08ac4e503ba009cfb8524b602cc1c3f2a4e
Muhammad Alhajee <alhajee2009@gmail.com>
2021-12-27 11:10:25 +0100

    fixed connecting to custom database

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = "mongodb://localhost:27017";
 
 // use when starting application as docker container
 let mongoUrlDocker = "mongodb://admin:password@mongodb";
@@ -9,8 +9,26 @@ let mongoUrlDocker = "mongodb://admin:password@mongodb";
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "crusty-db";
 
+// callback on successful connection to mongodb
+mongoose.connection.on("open", function(ref) {
+    console.log("Connected to mongo server.");
+});
+
+// callback on error connecting to mongodb
+mongoose.connection.on("error", function(err) {
+    console.log("Could not connect to mongo server!");
+    return console.log(err);
+});
+
+
 mongoose
-    .connect(`${mongoUrlLocal}`, { useNewUrlParser: true })
+    .connect(`${mongoUrlLocal}/${databaseName}`, 
+    {
+        authSource: "admin",
+        user: "admin",
+        pass: "password",
+        useNewUrlParser: false
+    })
     .catch(e => {
         console.error('Connection error', e.message)
     })
